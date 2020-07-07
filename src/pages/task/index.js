96133f7a77285e9c9f72998e26276053a5c1aa88
yiqiu <514499610@qq.com>
2020-07-07 19:04:36 +0800

    。

--- src/pages/task/index.js
+++ src/pages/task/index.js
@@ -1,7 +1,29 @@
 import React, { PureComponent } from 'react';
+import { Row, Col, Card, Statistic } from 'antd';
 
-export default class script extends PureComponent{
+export default class script extends PureComponent {
   render() {
-    return "task"
+    return (
+      <div>
+        <Row>
+          <Col span={18}>
+            <div style={{ height: 500 }}>
+              <Row>
+                <Col span={6}>
+                  <Card title={false} size="small" style={{ width: 300 }}>
+                    <div>
+                      <Statistic title="成功数" value={112893} />
+                    </div>
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col span={6}>
+            <div style={{ height: 500 }}></div>
+          </Col>
+        </Row>
+      </div>
+    );
   }
 }
